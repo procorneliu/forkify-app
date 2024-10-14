0a4324ed33175cc433c1c565c1323c56d0e1d1ba
procorneliu <procorneliu@gmail.com>
2024-10-14 17:17:25 +0300

    Problems with packages

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,5 +1,5 @@
 import icons from 'url:../../img/icons.svg';
-import { Fraction } from 'fractional';
+import Fraction from 'fraction.js';
 import View from './View.js';
 
 class RecipeView extends View {
@@ -111,7 +111,7 @@ class RecipeView extends View {
                 <svg class="recipe__icon">
                   <use href="${icons}#icon-check"></use>
                 </svg>
-                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+                <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toFraction(true) : ''}</div>
                 <div class="recipe__description">
                   <span class="recipe__unit">${ing.unit}</span>
                   ${ing.description}
