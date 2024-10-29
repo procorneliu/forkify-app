63144d3227a1c02bc1e9f268b0aba898755102ab
procorneliu <procorneliu@gmail.com>
2024-10-29 09:40:19 +0200

    fixed error of not uploading recipe when clicking from search result

--- src/js/controller.js
+++ src/js/controller.js
@@ -14,6 +14,7 @@ import { Chart } from 'chart.js/auto';
 
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { clearCanvas } from 'chart.js/helpers';
 
 // Hot module, reloading changes on page without page reload
 if (module.hot) {
