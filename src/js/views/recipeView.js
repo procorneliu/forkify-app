d3966a5d6a7f751704506a9d1e768e189e218535
procorneliu <procorneliu@gmail.com>
2024-10-29 10:31:09 +0200

    fixed error of not uploading recipe when clicking from search result 3

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -199,8 +199,6 @@ class RecipeView extends View {
 
   // Put all Nutrition Data in CHART format. Used extension: Chart.js
   generateNutritionChart() {
-    console.log(this);
-    console.log(this._data);
     const _canvas = document.getElementById('nutrition_chart');
 
     // Get rid of error reusing canvas
